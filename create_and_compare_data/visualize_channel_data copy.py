c3a4b0f65e5effb9c5ed6f57c1ca4f405245c0aa
Ni1Le2 <niconico.leng@googlemail.com>
2024-06-06 15:57:42 +0200

    First implementation of standard deviation of multiple (artificial) trials in V component plot

--- create_and_compare_data/visualize_channel_data copy.py
+++ create_and_compare_data/visualize_channel_data copy.py
@@ -74,6 +74,18 @@ def get_data_points(data, selected_channels=256, selected_time_points=369):
     else:
         print("No valid indices provided.")
 
+
+def open_h5(file_name,dataset_name):
+    file_path = './data/Somatosensory/HDF5/'
+    filename = file_path + file_name
+    dataset_name = dataset_name
+    with h5py.File(filename, 'r') as file:
+        data_set = file[dataset_name]
+        #make sure we only use the 369 time points of the data
+        data=data_set[:369]
+    return data
+
+
 def center(data):
     """
     Center the data by subtracting the mean of each channel.
@@ -113,7 +125,7 @@ def create_directory(path):
     if not os.path.exists(path):
         os.makedirs(path)
 
-def plot_butterfly(subject, data, save_path):
+def plot_butterfly(subject, data, save_path=None):
     """
     Plot the butterfly plot for the provided data and save it.
     
@@ -130,7 +142,8 @@ def plot_butterfly(subject, data, save_path):
 
     fig.tight_layout()
     plt.show()
-    fig.savefig(save_path + "butterfly_plot_all_channels.png", dpi=fig.dpi)
+    if save_path:
+        fig.savefig(save_path + "butterfly_plot_all_channels.png", dpi=fig.dpi)
 
 def plot_singular_values(subject, s, save_path):
     """
@@ -207,7 +220,7 @@ def plot_reconstruction(subject, U, s, V, save_path):
     plt.show()
     fig.savefig(save_path + "overview_svd_reconstruction.png")
 
-def plot_v_components(subject, V, s, V_uc, s_uc, save_path):
+def plot_v_components(subject, V, s, V_uc=None, s_uc=None, color_plot=False, save_path= None):
     """
     Plot the first three components of the V matrix and their phase space, and save the plot.
     
@@ -219,14 +232,16 @@ def plot_v_components(subject, V, s, V_uc, s_uc, save_path):
         s_uc (np.ndarray): The uncentered singular values.
         save_path (str): The path to save the plot.
     """
-    fig, axs = plt.subplots(2, 1)
     s1, s2, s3 = s[:3]
+    
     x_v = V[0, :] * s1
     y_v = V[1, :] * s2
     z_v = V[2, :] * s3
 
-    x_v_uc = V_uc[0, :] * s1
-    y_v_uc = V_uc[1, :] * s2
+    fig, axs = plt.subplots(2, 1)
+    
+    # Define a colormap that varies according to the index of data points
+    cmap = plt.get_cmap('Set1')
 
     axs[0].plot(x_v)
     axs[0].plot(y_v)
@@ -235,22 +250,102 @@ def plot_v_components(subject, V, s, V_uc, s_uc, save_path):
     axs[0].set_xlabel("Time")
     axs[0].set_ylabel("Amplitude")
     axs[0].legend(["first component", "second component", "third component"])
-
-    axs[1].plot(x_v, y_v)
+    if color_plot:
+        # Plot colored data points
+        for i in range(len(x_v)):
+            axs[1].plot(x_v[i:i+2], y_v[i:i+2], color=cmap(i/len(x_v)), alpha=0.7)
+    else:
+        axs[1].plot(x_v, y_v)
     axs[1].set_title(f"Phase space of first 2 components scaled by respective singular values (subject {subject})")
     axs[1].set_xlabel("First component of V")
     axs[1].set_ylabel("Second component of V")
 
-    axs[1].plot(x_v_uc, y_v_uc)
-    axs[1].legend(['Centered', 'Uncentered'])
+    if not(V_uc is None and s_uc is None):
+        s_uc1, s_uc2 = s_uc[:2]
+        x_v_uc = V_uc[0, :] * s_uc1
+        y_v_uc = V_uc[1, :] * s_uc2
 
-    fig.tight_layout()
+        if color_plot:
+            # Plot colored data points
+            for i in range(len(x_v_uc)):
+                axs[1].plot(x_v_uc[i:i+2], y_v_uc[i:i+2], color=cmap(i/len(x_v_uc)), alpha=0.7)
+        else:
+            axs[1].plot(x_v_uc, y_v_uc)
+            axs[1].legend(['Centered', 'Uncentered'])
+    
+    
+    # Colorbar for reference
+    sm_v = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(vmin=0, vmax=len(x_v)))
+    sm_v.set_array([])
+    plt.colorbar(sm_v, ax=axs.ravel().tolist())
+    #fig.tight_layout()
     plt.show()
-    fig.savefig(save_path + "v_components.png")
 
+    if save_path:
+        fig.savefig(save_path + "v_components.png")
+
+
+def plot_v_components_with_std(subject, mean_x, mean_y, std_x, std_y):
+    # same result, different look
+    #plt.errorbar(x=mean_x, y=mean_y, xerr=std_x,yerr=std_y)
+    #plt.show()
+
+    # covering the stds in all possible directions
+    plt.plot(mean_x, mean_y, "b-", linewidth=2)
+    plt.plot(mean_x-std_x, mean_y-std_y, "c--", linewidth=.5)
+    plt.plot(mean_x+std_x, mean_y-std_y, "c--", linewidth=.5)
+    plt.plot(mean_x-std_x, mean_y+std_y, "c--", linewidth=.5)
+    plt.plot(mean_x+std_x, mean_y+std_y, "c--", linewidth=.5)
+    plt.plot(mean_x-std_x, mean_y, "c--", linewidth=.5)
+    plt.plot(mean_x+std_x, mean_y, "c--", linewidth=.5)
+    plt.plot(mean_x, mean_y-std_y, "c--", linewidth=.5)
+    plt.plot(mean_x, mean_y+std_y, "c--", linewidth=.5)
+    plt.show()
+
+
+def plot_3D_v_components(subject, V, s):
+     ## V components
+    ax = plt.figure().add_subplot(projection='3d')
+
+    x_v = V[0,:]*s[0]
+    y_v = V[1,:]*s[1]
+    z_v = V[2,:]*s[2]
+
+    # Define a colormap that varies according to the index of data points
+    cmap = plt.get_cmap('Set1')
+
+    # Plot data points
+    for i in range(len(x_v)):
+        ax.plot(x_v[i:i+2], y_v[i:i+2], z_v[i:i+2], color=cmap(i/len(x_v)), alpha=0.7)
+
+    # Colorbar for reference
+    sm_v = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(vmin=0, vmax=len(x_v)))
+    sm_v.set_array([])
+    plt.colorbar(sm_v, ax=ax)
+    ax.set_title(f"Phase space of first 3 components of V-matrix (subject {subject})")
+    ax.set_xlabel("First component")
+    ax.set_ylabel("Second component")
+    ax.set_zlabel("Third component")
+    plt.show()
+
+
+def plot_3D_v_components_with_std(subject, mean_x, mean_y, mean_z, std_x, std_y, std_z):
+    ax = plt.figure().add_subplot(projection='3d')
+    # same idea, different look
+    #ax.errorbar(x=mean_x, y=mean_y, z=mean_z, xerr=std_x, yerr=std_y , zerr=std_z)
+    
+    ax.plot(mean_x, mean_y, mean_z ,  "b-", linewidth=2)
+    ax.plot(mean_x+std_x, mean_y, mean_z,  "c--", linewidth=1)
+    ax.plot(mean_x-std_x, mean_y, mean_z,  "c--", linewidth=1)
+    ax.plot(mean_x, mean_y+std_y, mean_z,  "c--", linewidth=1)
+    ax.plot(mean_x, mean_y-std_y, mean_z,  "c--", linewidth=1)
+    ax.plot(mean_x, mean_y, mean_z+std_z,  "c--", linewidth=1)
+    ax.plot(mean_x, mean_y, mean_z-std_z,  "c--", linewidth=1)
+    
+    plt.show()
 
 def main():
-    subject = "07"  # Replace with the appropriate subject identifier
+    subject = "06"  # Replace with the appropriate subject identifier
     
     file_path = './data/Somatosensory/PlosOne/' + subject + '_SEP_prepro_-50_130ms.csv'
     save_path = "./screenshots/sub" + subject + "/"
@@ -266,6 +361,7 @@ def main():
     logger.info("Extracting time points and signals...")
     # get uncentered signals
     time_points, uc_signals = get_data_points(org_avg_data)
+    
     if uc_signals is None:
         logger.error("No valid signals found.")
         return
@@ -278,12 +374,49 @@ def main():
 
     # Plot results
     logger.info("Plotting results...")
-    plot_butterfly(subject, signals, save_path)
-    plot_singular_values(subject, s, save_path)
-    plot_variance_explained(subject, s, save_path)
-    plot_reconstruction(subject, U, s, V, save_path)
-    plot_v_components(subject, V, s, V_uc, s_uc, save_path)
+    #plot_butterfly(subject, signals, save_path)
+    #plot_singular_values(subject, s, save_path)
+    #plot_variance_explained(subject, s, save_path)
+    #plot_reconstruction(subject, U, s, V, save_path)
+    #plot_v_components(subject, V, s, color_plot=True)
+    #plot_3D_v_components(subject, V, s)
 
+    # for noisy data
+    subject = "6 (noise)"
+    filename = "dataset_sub06_data_noise_val.h5"
+    ds_name = "data_noise_val"
+    all_noisy_x = []
+    all_noisy_y = []
+    all_noisy_z = []
+    for i in range(50):
+        uc_noisy_signals = open_h5(filename, ds_name + str(i)) 
+        noisy_signals = center(uc_noisy_signals)
+        
+        U_noisy, s_noisy, V_noisy = np.linalg.svd(noisy_signals, full_matrices=False)
+        #plot_v_components(subject, V_noisy, s_noisy, color_plot=True)
+        #plot_3D_v_components(subject, V_noisy, s_noisy)
+        
+        # noisy signal transformed to the space spanned by the V-matrix scaled by singular values of the average signal
+        noisy_signal_transformed = np.matmul(U.T, noisy_signals)
+        noisy_x, noisy_y, noisy_z = noisy_signal_transformed[:3,:]
+        all_noisy_x.append(noisy_x)
+        all_noisy_y.append(noisy_y)
+        all_noisy_z.append(noisy_z)
+
+    mean_noisy_x = np.mean(all_noisy_x, axis=0)
+    std_noisy_x = np.std(all_noisy_x, axis=0)
+    mean_noisy_y = np.mean(all_noisy_y, axis=0)
+    std_noisy_y = np.std(all_noisy_y, axis=0)
+    mean_noisy_z = np.mean(all_noisy_z, axis=0)
+    std_noisy_z = np.std(all_noisy_z, axis=0)
+    
+    plot_3D_v_components_with_std(subject, mean_noisy_x, mean_noisy_y, mean_noisy_z, std_noisy_x, std_noisy_y, std_noisy_z)
+    
+    plot_v_components_with_std(subject, mean_noisy_x, mean_noisy_y, std_noisy_x, std_noisy_y)
+
+    plot_v_components(subject, V=noisy_signal_transformed, s=np.ones(3), V_uc=noisy_signal_transformed, s_uc=np.ones(3), color_plot=True)
+    plot_3D_v_components(subject, noisy_signal_transformed, np.ones(3))
+    
     logger.info("All operations completed successfully.")
 
 if __name__ == "__main__":
